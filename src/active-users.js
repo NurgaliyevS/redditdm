da3e9b7da52ec93c9e08035890cfa852ad3aa417
NurgaliyevS <nurgasab@gmail.com>
2025-05-27 21:15:09 +0500

    upd

--- src/active-users.js
+++ src/active-users.js
@@ -259,11 +259,6 @@ async function getMostActiveUsers(subreddits) {
                   limit: 200000,
                 });
                 break;
-                posts = await subreddit.getNew({ 
-                  limit: POST_FETCH_CONFIG.chunkSize,
-                  after: offset > 0 ? allPosts[allPosts.length - 1]?.name : undefined,
-                });
-                break;
             }
 
             if (posts.length === 0) {
